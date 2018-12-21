8442a043c2613837686b4fc598a384574b2431e6
Ilmari <ilmari.koskinen1@gmail.com>
2018-12-21 13:24:07 +0200

    Rewrite service worker

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,55 @@
-var CACHE_NAME = 'workers-v1';
+const filesToCache = ['/', 'styles.css', 'app.js', 'index.html', 'favicon.ico'];
 
-self.addEventListener('fetch', function(event) {
+const staticCacheName = 'workers-cache-v1';
+
+self.addEventListener('install', event => {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      }
-
-      var fetchRequest = event.request.clone();
-
-      return fetch(fetchRequest).then(function(response) {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
+    caches
+      .match(event.request)
+      .then(response => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
           return response;
         }
-
-        var responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, responseToCache);
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(response => {
+          // TODO 5 - Respond with custom 404 page
+          return caches.open(staticCacheName).then(cache => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
         });
+      })
+      .catch(error => {
+        // TODO 6 - Respond with custom offline page
+      })
+  );
+});
 
-        return response;
-      });
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
+
+  const cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
